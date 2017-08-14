5992ab2056894c0df524421e8877b544a5f42aba
Stefan Iurasog <office@green-art.ro>
2017-08-14 15:38:00 +0300

    m2_greenart_breadcrumbs

---
+++ GreenArt/Breadcrumbs/Block/Html/Breadcrumbs.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * GreenArt_Breadcrumbs extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE_EASYLIFE_BREADCRUMBS.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category       GreenArt
+ * @package        GreenArt_Breadcrumbs
+ * @author         Stefan Iurasog
+ * @email          office[at]green-art.ro
+ * @copyright      Copyright (c) 2017
+ * @license        http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace GreenArt\Breadcrumbs\Block\Html;
+
+/**
+ * Class Breadcrumbs
+ * @package GreenArt\Breadcrumbs\Block\Html
+ */
+class Breadcrumbs extends \Magento\Theme\Block\Html\Breadcrumbs {
+	/**
+	 * @todo Here you can extend Breadcrumbs
+	 */
+}
---
+++ GreenArt/Breadcrumbs/registration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * GreenArt_Breadcrumbs extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE_EASYLIFE_BREADCRUMBS.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category       GreenArt
+ * @package        GreenArt_Breadcrumbs
+ * @author         Stefan Iurasog
+ * @email          office[at]green-art.ro
+ * @copyright      Copyright (c) 2017
+ * @license        http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'GreenArt_Breadcrumbs',
+    __DIR__
+);
